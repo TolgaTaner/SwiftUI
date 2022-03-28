3bc4dd9957814f06fe87d7288f0a2a31a4554161
tolga <dev.tolgataner@icloud.com>
2022-03-28 20:14:42 +0300

    delete unused geometry reader and change to deployment target to 15.0

--- Authentication/Authentication/ContentView.swift
+++ Authentication/Authentication/ContentView.swift
@@ -19,39 +19,37 @@ struct RegisterView: View {
     }
     
     var body: some View {
-        GeometryReader { proxy in
+        VStack(alignment: .center, spacing: 20) {
+            Spacer()
             VStack(alignment: .center, spacing: 20) {
-                Spacer()
-                VStack(alignment: .center, spacing: 20) {
-                    TextField(Keys.mailAddress, text: $mailAddress, onCommit: {
-                        print(mailAddress)
-                    })
-                        .frame(height: 44)
-                        .textFieldStyle(PlainTextFieldStyle())
-                        .padding([.horizontal], 4)
-                        .cornerRadius(16)
-                        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray))
-                        .padding([.horizontal], 24)
-                        .submitLabel(.continue)
-                    
-                    SecureField(Keys.password, text: $password)
-                        .frame(height: 44)
-                        .textFieldStyle(PlainTextFieldStyle())
-                        .padding([.horizontal], 4)
-                        .cornerRadius(16)
-                        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray))
-                        .padding([.horizontal], 24)
-                        .submitLabel(.done)
-                }
-                Spacer()
-                Spacer()
-                Spacer()
-                Button(Keys.button) {
-                    
-                }.frame(width: UIScreen.main.bounds.width, height: 44)
+                TextField(Keys.mailAddress, text: $mailAddress, onCommit: {
+                    print(mailAddress)
+                })
+                    .frame(height: 44)
+                    .textFieldStyle(PlainTextFieldStyle())
+                    .padding([.horizontal], 4)
+                    .cornerRadius(16)
+                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray))
+                    .padding([.horizontal], 24)
+                    .submitLabel(.continue)
+                
+                SecureField(Keys.password, text: $password)
+                    .frame(height: 44)
+                    .textFieldStyle(PlainTextFieldStyle())
+                    .padding([.horizontal], 4)
+                    .cornerRadius(16)
+                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray))
+                    .padding([.horizontal], 24)
+                    .submitLabel(.done)
+            }
+            Spacer()
+            Spacer()
+            Spacer()
+            Button(Keys.button) {
+                
+            }.frame(width: UIScreen.main.bounds.width, height: 44)
                 .background(Color.red)
                 .foregroundColor(.white)
-            }
         }
     }
 }
