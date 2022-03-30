51368661cb63cb060c7aedd53f065b78e366b3a5
tolga <dev.tolgataner@icloud.com>
2022-03-30 18:16:24 +0300

    add rounded border style view modifier

---
+++ Authentication/Authentication/RoundedBorderTextFieldStyle.swift
@@ -0,0 +1,24 @@
+//
+//  RoundedBorderTextFieldStyle.swift
+//  Authentication
+//
+//  Created by Tolga Taner on 30.03.2022.
+//
+
+import SwiftUI
+
+struct RoundedBorderTextFieldStyle: ViewModifier {
+    func body(content: Content) -> some View {
+         content
+            .padding([.horizontal], 4)
+            .cornerRadius(16)
+            .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray))
+            .padding([.horizontal], 24)
+     }
+}
+
+extension TextField {
+    func roundedBorderTextFieldStyle() -> some View {
+        modifier(RoundedBorderTextFieldStyle())
+    }
+}
